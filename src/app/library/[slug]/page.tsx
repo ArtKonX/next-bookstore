836dd6765280e4063bd091066b67d69c7010c221
Artemij Kondurov <kondurovartemij@yandex.ru>
2024-08-03 16:02:27 +0300

    Unnecessary code has been deleted

--- src/app/library/[slug]/page.tsx
+++ src/app/library/[slug]/page.tsx
@@ -5,20 +5,6 @@ import { useEffect, useState } from "react";
 
 import IBook from "@/interfaces/book.interface";
 
-// export async function getStaticPaths() {
-//     const response = await getBooksData();
-//     const books = response.data;
-
-//     const paths = books.map((book: IBook) => ({
-//         params: { slug: book?._id.toString() },
-//     }));
-
-//     return {
-//         paths,
-//         fallback: false,
-//     };
-// }
-
 type Params = {
     slug: string
 }
